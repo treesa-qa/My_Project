d6fb311eb4609e0b977f7b489ba649779f5f19b0
treesa-qa <treesarusselraj@gmail.com>
2025-06-12 06:25:59 +0530

    second commit

---
+++ My_Java_Code/src/string_functions/String_Replace.java
@@ -0,0 +1,12 @@
+package string_functions;
+
+public class String_Replace {
+
+	public static void main(String[] args) {
+		String s1="Treesa russel raj";
+	System.out.println(	s1.replace('e', 'i'));
+		
+
+	}
+
+}
