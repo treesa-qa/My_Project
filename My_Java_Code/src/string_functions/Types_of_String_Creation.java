4784a0eabd66ffcc7e8e62d4ec102cfe78a7de6a
treesa-qa <treesarusselraj@gmail.com>
2025-06-12 05:34:00 +0530

    Intial commit with sample java program

---
+++ My_Java_Code/src/string_functions/Types_of_String_Creation.java
@@ -0,0 +1,13 @@
+package string_functions;
+
+public class Types_of_String_Creation {
+	
+
+	public static void main(String[] args) {
+		
+
+		String s1=new String("This is my sample string");
+		System.out.print(s1);
+	}
+
+}
